42300563726c1cbfdd9bc2614eac872dab76f36b
Ildana <ildana.ruzybayeva@gmail.com>
2020-08-24 18:04:24 +0000

    Kick and ban users bot

--- src/index.js
+++ src/index.js
@@ -1,21 +1,53 @@
 const express = require("express");
-require("dotenv").config();
 const app = express();
 const port = 3000;
-const { Client } = require("discord.js");
+require("dotenv").config();
 
+const { Client } = require("discord.js");
 const client = new Client();
+const PREFIX = "$";
+
 client.login(process.env.DISCORD_BOT_TOKEN);
 
-client.on("message", message => {
-  if (!message.content) return;
-  if (message.content.includes("ildana")) {
-    message.reply("why u say my name?");
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+    if (CMD_NAME === "kick") {
+      if (!message.member.hasPermission("KICK_MEMBERS"))
+        return message.reply("You do not have permissions to use that command");
+      if (args.length === 0) return message.reply("Please provide an ID");
+      const member = message.guild.members.cache.get(args[0]);
+      if (member) {
+        member
+          .kick()
+          .then(member => message.channel.send(`${member} was kicked.`))
+          .catch(err => message.channel.send("I cannot kick that user :("));
+      } else {
+        message.channel.send("That member was not found");
+      }
+    } else if (CMD_NAME === "ban") {
+      if (!message.member.hasPermission("BAN_MEMBERS"))
+        return message.reply("You do not have permissions to use that command");
+      if (args.length === 0) return message.reply("Please provide an ID");
+      try {
+        const user = await message.guild.members.ban(args[0]);
+        message.channel.send("User was banned successfully");
+      } catch (err) {
+        console.log(err);
+        message.channel.send(
+          "An error occured. Either I do not have permissions or the user was not found"
+        );
+      }
+    }
   }
 });
 
 app.get("/", (req, res) => {
-  res.send("hi");
+  res.send("hi this is a discord bot app");
 });
 
 app.listen(port, () => {
